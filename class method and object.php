f4e35fa8fd8adc545038a93957ee4bfa6dfbaf00
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2018-10-29 06:10:48 +0600

    class,method

---
+++ class method and object.php
@@ -0,0 +1,17 @@
+<?php
+	class Person{
+		public $name;
+		public $age;
+		public function personName(){
+			echo "The person name is :".$this->name."<br/>";
+		}
+		public function personAge($value){
+			echo "The person age is:".$this->age=$value;;
+		}
+	}
+	$personOne = new Person();
+    $value=$personOne->name="joynal Abedin";
+	echo $value."<br/>";
+	$personOne->personName();
+	$personOne->personAge("22");
+?>
